283e209c9bf08b3240aa9bb6b7808baf474d5aa5
aajjbb <jefersonlsiq@gmail.com>
2017-06-20 01:35:44 -0300

    adicionando rotina para ler um arquivo de entrada e transformalo em uma lista de bitsets

--- src/br/ita/toner/ga/data/DataLoader.java
+++ src/br/ita/toner/ga/data/DataLoader.java
@@ -3,6 +3,9 @@ package br.ita.toner.ga.data;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.BitSet;
+import java.util.List;
 
 import br.ita.toner.ga.individual.SparseMatrix;
 
@@ -36,5 +39,36 @@ public class DataLoader {
 		}
 		
 		return matrix;
+	}
+	
+public List<BitSet> loadFileAsBitsetList(String filePath) {		
+		try(BufferedReader br = new BufferedReader(new FileReader(new File(filePath)))) {
+			
+			String line = br.readLine();
+			
+			int n = Integer.parseInt(line.split(" ")[0]);			
+			int m = Integer.parseInt(line.split(" ")[1]);	
+			
+			List<BitSet> answer = new ArrayList<>();
+						
+		    for(int i = 0; (line = br.readLine()) != null; i++) {		    	
+		    	String[] numbers = line.split(" ");
+		    	
+		    	BitSet curr_line = new BitSet(m);
+		    	
+		    	for (int j = 0; j < numbers.length; j++) {
+		    		if (numbers[j].equals("1")) {
+		    			curr_line.set(j);
+		    		}
+		    	}
+		    	
+		    	answer.add(curr_line);
+		    }
+		    
+			return answer;
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new RuntimeException("Failed to load input file");
+		}
 	}
 }
