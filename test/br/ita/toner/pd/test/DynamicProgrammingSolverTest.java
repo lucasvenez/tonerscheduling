0513391a95be63cebff073f45971030847322cbc
aajjbb <jefersonlsiq@gmail.com>
2017-06-22 18:22:10 -0300

    concertando testes da pd

--- test/br/ita/toner/pd/test/DynamicProgrammingSolverTest.java
+++ test/br/ita/toner/pd/test/DynamicProgrammingSolverTest.java
@@ -20,32 +20,32 @@ public class DynamicProgrammingSolverTest {
 		
 		DynamicProgrammingSolver solver = new DynamicProgrammingSolver(input);	
 
-		assertEquals(5, solver.getSolutionAsInt());
+		assertEquals(6, solver.getSolutionAsInt());
 	}
 	
-//	@Test
-//	public void testListOutput() {
-//		DataLoader loader = new DataLoader();
-//		
-//		TestCase input = loader.loadFileAsBitsetList("/home/aajjbb/Devel/tonerscheduling/resources/1.txt");
-//		
-//		DynamicProgrammingSolver solver = new DynamicProgrammingSolver(input);	
-//		
-//		List<Integer> order = solver.getSolutionAsList();
-//		List<Integer> right_output = new ArrayList<>();
-//		
-//		right_output.add(0);
-//		right_output.add(1);
-//		right_output.add(3);
-//		right_output.add(4);
-//		right_output.add(5);
-//		right_output.add(6);
-//		right_output.add(7);
-//		right_output.add(8);
-//		right_output.add(9);
-//		right_output.add(10);
-//		right_output.add(2);
-//		
-//		assertEquals(right_output, order);
-//	}
+	@Test
+	public void testListOutput() {
+		DataLoader loader = new DataLoader();
+		
+		TestCase input = loader.loadFileAsBitsetList("/home/aajjbb/Devel/tonerscheduling/resources/1.txt");
+		
+		DynamicProgrammingSolver solver = new DynamicProgrammingSolver(input);	
+		
+		List<Integer> order = solver.getSolutionAsList();
+		List<Integer> right_output = new ArrayList<>();
+		
+		right_output.add(0);
+		right_output.add(1);
+		right_output.add(3);
+		right_output.add(4);
+		right_output.add(6);
+		right_output.add(5);
+		right_output.add(7);
+		right_output.add(8);
+		right_output.add(2);
+		right_output.add(9);
+		right_output.add(10);
+		
+		assertEquals(right_output, order);
+	}
 }
